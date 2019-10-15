1b0f822d2f661cd53de37416dfed966b667a28b8
fengzhenquan <fengzhenquan@kingsoft.com>
2019-10-15 22:03:03 +0800

    1

--- node-express-ts-api/src/components/Reptile/Reptile/jobReptile.ts
+++ node-express-ts-api/src/components/Reptile/Reptile/jobReptile.ts
@@ -7,7 +7,7 @@ charset(superagent);
 var async = require('async'); //异步抓取
 var eventproxy = require('eventproxy');  //流程控制
 var ep = eventproxy();
-let baseUrl:string="https://www.zhipin.com/c101010100/?query=web%E5%89%8D%E7%AB%AF&page=1&ka=page-1";
+let baseUrl:string="https://www.zhipin.com/c101010100/?query=%E5%89%8D%E7%AB%AF&page=1&ka=page-1";
 
 
 /**
@@ -17,17 +17,17 @@ let baseUrl:string="https://www.zhipin.com/c101010100/?query=web%E5%89%8D%E7%AB%
  * @returns {Promise < void >}
  */
 export async function jobReptile(): Promise < void > {
-      
     superagent.get(baseUrl)
     .charset('gb2312')
     .end(function (err:any, sres:any) {
+        
         // 常规的错误处理
         if (err) {
             console.log('抓取'+baseUrl+'这条信息的时候出错了')
         }
         var $ = cheerio.load(sres&&sres.text);
         var nextUrl=$('.page a').attr('href');//下一页Url
-        var linkElem:any=$('.job-list ul li');//当前UrlLits
-        console.log(linkElem);
+        var linkElem:any=$('.job-box .job-list ul li');//当前UrlLits
+        console.log(linkElem.length);
     });
 }
